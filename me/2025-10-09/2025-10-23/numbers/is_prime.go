048b96db08a98deef7667cf0ecc0866ed41a6a8c
sebastianhld <sebastian.jmheld@gmail.com>
2025-10-28 15:16:28 +0100

    ok

---
+++ me/2025-10-09/2025-10-23/numbers/is_prime.go
@@ -0,0 +1,15 @@
+package numbers
+
+// Erwartet eine Zahl n und prüft, ob n eine Primzahl ist.
+func IsPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
